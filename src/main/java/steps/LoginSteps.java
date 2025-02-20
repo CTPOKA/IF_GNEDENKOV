8109a73b535bd53c2f00308a7e8fa202e1b6ce14
Daniil <gnedenkov777daniil@gmail.com>
2025-02-20 09:03:00 +0500

    fix

--- src/main/java/steps/LoginSteps.java
+++ src/main/java/steps/LoginSteps.java
@@ -1,31 +1,16 @@
 package steps;
 
+import config.ConfigReader;
 import io.cucumber.java.ru.*;
 import org.junit.jupiter.api.Assertions;
 import pages.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
-
 public class LoginSteps {
-    private static final Properties props = new Properties();
-
-    static {
-        try (InputStream input = Files.newInputStream(Paths.get("src/main/java/config/Config.properties"))) {
-            props.load(input);
-        } catch (IOException e) {
-            throw new RuntimeException("Ошибка загрузки конфигурации", e);
-        }
-    }
-
     private final LoginPage jiraLoginPage = new LoginPage();
     private final MainPage jiraMainPage = new MainPage();
 
-    private final String login = props.getProperty("login");
-    private final String password = props.getProperty("password");
+    private final String login = ConfigReader.getProperty("login");
+    private final String password = ConfigReader.getProperty("password");
 
     @Допустим("пользователь заходит в Jira")
     public void loginToJira() {
--- src/test/java/webHooks/CucumberHooks.java
+++ src/test/java/webHooks/CucumberHooks.java
@@ -2,37 +2,23 @@ package webHooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import config.ConfigReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class CucumberHooks {
-    private static final Properties props = new Properties();
-
-    static {
-        try (InputStream input = Files.newInputStream(Paths.get("src/main/java/config/Config.properties"))) {
-            props.load(input);
-        } catch (IOException e) {
-            System.err.println("Ошибка загрузки конфигурации: " + e.getMessage());
-        }
-    }
 
     @Before("@jira")
     public void initBrowser() {
         Configuration.timeout = 15000;
-        Configuration.browser = props.getProperty("browser");
+        Configuration.browser = ConfigReader.getProperty("browser");
 
         Selenide.open();
         getWebDriver().manage().window().maximize();
 
-        Selenide.open(props.getProperty("url"));
+        Selenide.open(ConfigReader.getProperty("url"));
     }
 
     @After("@jira")
